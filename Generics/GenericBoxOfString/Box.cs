7da8f76fde54f0a48704341ee227f3abb252b420
mindball <mincho.balaliew@gmail.com>
2019-12-30 09:05:26 +0200

    Generic Box of String and Integert

---
+++ Generics/GenericBoxOfString/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.Value}";
+        }
+    }
+}
---
+++ Generics/GenericBoxOfString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int iteration = int.Parse(input);
+            
+
+            while(iteration > 0 )
+            {
+                input = Console.ReadLine();                
+
+                CallMyBoxClass(input);
+                
+                iteration--;
+            }
+        }
+
+        private static void CallMyBoxClass(string input)
+        {
+            if(int.TryParse(input, out _))
+            {
+                Box<int> box = new Box<int>(int.Parse(input));
+                Console.WriteLine(box);
+            }           
+            else
+            {
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+
+        }
+    }
+}
