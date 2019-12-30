e45830f0d333ae2990a46f4d0b5e0ff015a7f426
mindball <mincho.balaliew@gmail.com>
2019-12-30 09:52:10 +0200

    Generic Swap Method String and Integers

--- Generics/GenericSwapMethodStrings/Program.cs
+++ Generics/GenericSwapMethodStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericSwapMethodStrings
 {
@@ -6,7 +7,55 @@ namespace GenericSwapMethodStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            int count = int.Parse(input);
+            List<string> stringList = new List<string>();
+            List<int> intList = new List<int>();
+            while (count > 0)
+            {
+                input = Console.ReadLine();
+                int value;
+                if (int.TryParse(input, out value))
+                {                    
+                    intList.Add(value);
+                }
+                else
+                {                    
+                    stringList.Add(input);
+                }
+
+                count--;
+            }
+
+            string[] index = Console.ReadLine().Split(" ");
+            int firstIndex = int.Parse(index[0]);
+            int secondIndex = int.Parse(index[1]);
+
+            if (stringList.Count != 0)
+            {
+                SwapIndexes(stringList, firstIndex, secondIndex);               
+            }
+            else
+            {
+                SwapIndexes(intList, firstIndex, secondIndex);
+            }
+
         }
+
+        public static void SwapIndexes<T>(List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+
+            var type = typeof(T);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{type}: {item}");
+            }
+        }
+
+        
     }
 }
